a0410b6e7c1bc585c570b1a3482cd4f7c1e60030
tnlvof <dlwhdtn10!>
2021-04-23 11:35:29 +0900

    initail Commit

---
+++ app/common/config.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass, asdict
+from os import path, environ
+
+base_dir = path.dirname(path.dirname(path.dirname(path.abspath(__file__))))
+
+
+@dataclass
+class Config:
+    """
+    기본 configuration
+    """
+    BASE_DIR = base_dir
+
+    DB_POOL_RECYCLE: int = 900
+    DB_ECHO: bool = True
+
+
+@dataclass
+class LocalConfig(Config):
+    PROJ_RELOAD: bool = True
+
+
+@dataclass
+class ProduCofig(Config):
+    PROJ_RELOAD: bool = True
+
+
+@dataclass
+class ProdConfig(Config):
+    PROJ_RELOAD: bool = False
+
+
+def conf():
+    """
+    환경 불러오기
+    :return:
+    """
+    config = dict(prod=ProdConfig(), local=LocalConfig())
+    return config.get(environ.get("API_ENV", "local"))
+
+
+print(asdict(LocalConfig()))
